9b64b22e79db3b477bdb8332045b38a80445f9ff
Brandon Jaipersaud <brandon.jaipersaud@mail.utoronto.ca>
2022-06-09 11:52:25 -0400

    Add export posts json to  separate python script

---
+++ export_posts_json.py
@@ -0,0 +1,108 @@
+import json
+import time
+import math
+from datetime import datetime
+import os
+from tqdm import trange, tqdm
+import re
+from copy import deepcopy
+import sys
+
+
+import csv
+import unicodedata
+import html
+from io import StringIO
+from html.parser import HTMLParser
+
+
+import pandas as pd
+from pandas import DataFrame, Series
+import numpy as np
+from numpy import ndarray 
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+
+from sklearn.feature_selection import SelectKBest
+from sklearn.feature_selection import mutual_info_classif
+from sklearn.feature_selection import chi2
+
+
+
+
+from nptyping import NDArray, Int, Shape
+from typing import Dict, List, Tuple, Union
+
+
+from transformers import pipeline
+from transformers import AutoModelForSequenceClassification
+from transformers import AutoTokenizer
+import nltk
+import ssl
+from nltk.tokenize import word_tokenize
+from collections import namedtuple
+
+
+from piazza_api import Piazza
+from piazza_api.network import Network
+
+
+
+def login(email:str, password:str, courseid: str) -> Tuple[dict, Network]:
+    """logs user into Piazza"""
+
+    p: Piazza = Piazza()
+    p.user_login(email, password)
+    user_profile: dict = p.get_user_profile()
+    course: Network = p.network(courseid)
+    return user_profile, course
+
+
+
+def export_posts_json(filename:str, course:Network, max_iters='all') -> None:
+    """Create json of all posts saved in current directory"""
+
+    if os.path.exists(filename):
+        print(f"{filename} already exists!")
+        return 
+    posts = course.iter_all_posts()
+    all_posts = []
+    #text = json.dumps(post['children'][1], sort_keys=True, indent=4)
+    try:
+        iters = 0
+        for p in tqdm(posts):
+            time.sleep(2.5)
+            all_posts.append(p)
+            iters += 1
+            if iters == max_iters and max_iters != 'all':
+                break
+    finally:
+        print('------------------------------------')
+        with open(filename, 'w') as f:
+            json.dump(all_posts, f)
+
+
+
+
+def print_usage():
+    usage = "Usage: python3.9 export_posts_json.py <cred-file-path> <save-json-filename>"
+    print(usage)
+
+
+def main():
+    if len(sys.argv) != 3:
+        print_usage()
+        return
+    cred_file_path = sys.argv[1]
+    filename = sys.argv[2]
+    with open(cred_file_path) as f:
+        creds = json.load(f)
+        email, password, courseid = creds['email'], creds['password'], creds['courseid']
+        user_profile, course = login(email, password, courseid)
+        export_posts_json(filename, course)
+
+
+if __name__ == "__main__":
+    main()
